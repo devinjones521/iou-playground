07dc7213c589a830b6cadbe10bc932effc2e126b
devinjones521 <devinjones521@gmail.com>
2026-09-12 13:48:04 +0100

    Refactor fetchUser return

--- src/users.js
+++ src/users.js
@@ -2,7 +2,8 @@
 export async function fetchUser(id) {
   const res = await fetch(`https://api.example.com/users/${id}`);
   if (!res.ok) throw new Error(`fetchUser failed: ${res.status}`);
-  return res.json();
+  const data = await res.json();
+  return data;
 }
 
 export function formatName(user) {
